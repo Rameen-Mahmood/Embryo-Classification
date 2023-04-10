651226d5b4df4f579b61e31f95eddd65efcdcc8a
yaya1721 <Sonyahyun.hs@gmail.com>
2023-04-10 08:46:23 +0400

    preprocessing

--- ml4h_ivf/datasets/embryo_public.py
+++ ml4h_ivf/datasets/embryo_public.py
@@ -124,10 +124,10 @@ def merge_df(all_df_img, all_df_ann):
 
 def get_transforms(args):
     train_transforms = transforms.Compose([
-        transforms.Resize((400, 400)),
-        transforms.CenterCrop(224),
+        transforms.Resize(224),
+        #transforms.CenterCrop(224),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
+        #transforms.RandomVerticalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
